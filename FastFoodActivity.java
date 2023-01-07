abac0fccaf47296e291f1bd5916e64074827dbaa
chernhaw <38425578+chernhaw@users.noreply.github.com>
2023-01-07 17:56:17 +0800

    Add files via upload

---
+++ FastFoodActivity.java
@@ -0,0 +1,14 @@
+package com.example.bendemeermakan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FastFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fastfood);
+    }
+}
